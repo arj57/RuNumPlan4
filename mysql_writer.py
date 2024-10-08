5652159aa04693ada912d65dac039402cb562892
Alex Y <ay@smartkom.ru>
2024-10-08 12:39:31 +0600

    MysqlWriter: добавил сохранение метаданных

--- mysql_writer.py
+++ mysql_writer.py
@@ -1,6 +1,3 @@
-# import typing
-# from datetime import datetime
-
 from mysql.connector.cursor import MySQLCursor
 
 import app_logger
@@ -8,7 +5,6 @@ import mysql
 from overrides import override
 import config
 import data_types
-# from metadata import AbstractMetadata
 from singleton_db_connector import DbConnector
 from abstract_writer import AbstractWriter
 
@@ -57,19 +53,18 @@ class MysqlWriter(AbstractWriter):
         self.store_rows(data.rows)
         self.store_loc_ref(data.loc_objects)
         self.store_op_ref(data.op_id_titles)
-        self.store_metadata()
+        self.store_metadata(data.metadata)
 
         if bool(self.config.get_param_val("./DryRun")):
             logger.info("Dry run, not commited")
         else:
-            # meta.set_last_row_id()
-            # meta.save()
             self._commit()
 
     def store_rows(self, rows: data_types.OutRows) -> None:
         cursor = self.conn.cursor()
         fields_list: list[str] = self.get_fields_list()
         try:
+            cursor.execute("TRUNCATE op_data")
             ins_query = " INSERT INTO op_data (" + ",".join(fields_list) + ") VALUES(" + (
                     "%s," * len(fields_list)).strip(",") + ")"
 
@@ -110,55 +105,14 @@ class MysqlWriter(AbstractWriter):
         finally:
             cursor.close()
 
-    def store_metadata(self):
-        pass
-
-# class MysqlMetadata(AbstractMetadata):
-#     def __init__(self, data: data_types.InpData, connection: mysql.connector.MySQLConnection):
-#         super().__init__(data)
-#         self.connection = connection
-#         self.first_record_id = self.__get_last_row_id() + 1
-#         self.last_record_id = None
-#
-#         logger.debug("first_record_id: %d" % self.first_record_id)
-#
-#     def __get_last_row_id(self) -> int:
-#         cur_3: MySQLCursor = self.connection.cursor()
-#         try:
-#             cur_3.execute("SELECT MAX(id) FROM cdrs")
-#             record_id = cur_3.fetchone()
-#             if record_id[0] is None:
-#                 return 0
-#             else:
-#                 return record_id[0]
-#         finally:
-#             cur_3.close()
-#
-#     def set_last_row_id(self) -> None:
-#         self.last_record_id = self.__get_last_row_id()
-#         logger.debug("last_record_id: %d" % self.last_record_id)
-#
-#     @override()
-#     def save(self) -> None:
-#         metadata: data_types.TMetadata = self.get_metadata()
-#         fields = metadata.keys()
-#         vals = tuple(metadata.values())
-#         query = ("INSERT INTO metadata (" + ",".join(fields)
-#                  + ") VALUES(" + ("%s," * len(fields)).strip(",") + ")")
-#
-#         cur2: MySQLCursor = self.connection.cursor()
-#         try:
-#             cur2.execute(query, vals)
-#         except mysql.connector.Error:
-#             self.connection.rollback()
-#             raise IOError
-#             # print(traceback.format_exc())
-#         finally:
-#             cur2.close()
-
-# @override()
-# def get_metadata(self) -> data_types.TMetadata:
-#     res = super().get_metadata()
-#     res['start_id'] = self.first_record_id
-#     res['end_id'] = self.last_record_id
-#     return res
+    def store_metadata(self, meta: data_types.Metadata):
+        cursor = self.conn.cursor()
+        try:
+            ins_query = "INSERT INTO metadata (filename, dt_created) VALUES( %s, %s)"
+            cursor.execute(ins_query, meta)
+        except mysql.connector.Error as er:
+            logger.error('Fail to insert loc_ref: %s' % er)
+            self._rollback()
+            raise IOError
+        finally:
+            cursor.close()
