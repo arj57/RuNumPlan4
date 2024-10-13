53903038107a4dd1c0bf4dce914b8a464f555751
Alexsander Yaryzhenko <ay@smartkom.ru>
2024-10-13 20:30:59 +0600

    Рефакторинг. Упрощение.

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ from abstract_reader import AbstractReader
 from data_types import OutData, InpData
 from num_plan_converter import NumPlanConverter
 from abstract_writer import AbstractWriter
-from singleton_git_repo import GitRepo
 from url import Url
 from git import Repo
 
@@ -23,9 +22,9 @@ logger = app_logger.get_logger(__name__)
 class RuNumPlan4:
     def __init__(self):
         self.conf = Config()
+        self.repo = self._get_git_repo()
 
-    @staticmethod
-    def reader_factory(config: Config, src_url: Url) -> AbstractReader:
+    def reader_factory(self, src_url: Url) -> AbstractReader:
         src_url_scheme: str = src_url.scheme
         mod_name = src_url_scheme + '_reader'
         class_name = src_url_scheme.capitalize() + 'Reader'
@@ -34,11 +33,10 @@ class RuNumPlan4:
             reader_class = dict(inspect.getmembers(mod, inspect.isclass))[class_name]
         except IndexError:
             raise AttributeError('Не найден класс "%s"' % class_name)
-        return reader_class(config, src_url)
+        return reader_class(self.conf, src_url)
 
-    @staticmethod
-    def writer_factory(config: Config) -> AbstractWriter:
-        dst_url_scheme: str = Url(config.get_param_val(r"Dst/Url")).scheme
+    def writer_factory(self) -> AbstractWriter:
+        dst_url_scheme: str = Url(self.conf.get_param_val(r"Dst/Url")).scheme
         mod_name = dst_url_scheme + '_writer'
         class_name = dst_url_scheme.capitalize() + 'Writer'
         mod = importlib.import_module(mod_name)
@@ -46,7 +44,7 @@ class RuNumPlan4:
             writer_class = dict(inspect.getmembers(mod, inspect.isclass))[class_name]
         except Exception:
             raise AttributeError('Не найден класс "%s"' % class_name)
-        return writer_class(config)
+        return writer_class(self.conf)
 
     def process(self) -> None:
         src_data: InpData = self.get_accumulated_inp_data(skip_header=True)
@@ -54,11 +52,9 @@ class RuNumPlan4:
             logger.info("Получены новые данные.")
             converter: AbstractConverter = NumPlanConverter(self.conf)
             dst_data: OutData = converter.get_converted_data(src_data)
-            dst: AbstractWriter = self.writer_factory(self.conf)
+            dst: AbstractWriter = self.writer_factory()
             dst.store_data(dst_data)
             dst.close()
-            self.store_data_to_repo()
-            # TODO: store new data to repo
             self.store_data_to_repo()
         else:
             logger.info("Данные не изменились. Нечего сохранять.")
@@ -69,25 +65,24 @@ class RuNumPlan4:
 
         src_urls: list[Url] = [Url(i.text) for i in self.conf.findall(r"Src/Urls/Url")]
         for i, url in enumerate(src_urls):
-            src: AbstractReader = self.reader_factory(self.conf, url)
+            src: AbstractReader = self.reader_factory(url)
             inp: data_types.InpData = src.get_parsed_data(skip_header=(skip_header or i > 0))
             records += inp.records
             meta += inp.metadata
             src.close()
         return data_types.InpData(metadata=meta, records=records)
 
-    def is_data_changed(self) -> bool:
+    def _get_git_repo(self) -> Repo:
         tmp_inp_dir = self.conf.get_param_val(r'InputDataDir')
         abs_tmp_inp_dir = utils.get_abs_path(tmp_inp_dir)
-        repo: Repo = GitRepo.get_instance(abs_tmp_inp_dir)
-        return repo.is_dirty()
+        return Repo(abs_tmp_inp_dir)
+
+    def is_data_changed(self) -> bool:
+        return self.repo.is_dirty()
 
     def store_data_to_repo(self) -> None:
         logger.info("Сохранение данных в репозитории...")
-        tmp_inp_dir = self.conf.get_param_val(r'InputDataDir')
-        abs_tmp_inp_dir = utils.get_abs_path(tmp_inp_dir)
-        repo: Repo = GitRepo.get_instance(abs_tmp_inp_dir)
-        idx = repo.index
+        idx = self.repo.index
         changed_files: list[str] = [item.a_path for item in idx.diff(None)]
         idx.add(changed_files)
         idx.commit(datetime.now().strftime("%Y.%m.%d %H:%M:%S"))
